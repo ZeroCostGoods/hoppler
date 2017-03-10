e5aeb2d2f9914c11fb477199548ce1500cb2d295
Digant Kasundra <digant@dropbox.com>
2017-03-10 15:25:17 -0800

    Initial stable POST of events

--- src/hopplerdb.rs
+++ src/hopplerdb.rs
@@ -1,13 +1,7 @@
-// What are my third party libraries?
-// What are my own modules: i.e. *.rs files not lib or main?
-
-
-// What do I need to import for this file?
 use diesel::prelude::*;
 use diesel::mysql::MysqlConnection;
 use dotenv::dotenv;
 use std::env;
-
 
 pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,17 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    events {
+        id -> Integer,
+        timestamp -> BigInt,
+        session_id -> VarChar,
+        time_on_page -> Nullable<Integer>,
+        username -> VarChar,
+        event_type -> VarChar,
+        hostname -> VarChar,
+        pathname -> VarChar,
+        search -> Nullable<VarChar>,
+        in_focus -> Bool,
+        prior_hostname -> Nullable<VarChar>,
+        prior_pathname -> Nullable<VarChar>,
+        prior_search -> Nullable<VarChar>,
+    }
+}
