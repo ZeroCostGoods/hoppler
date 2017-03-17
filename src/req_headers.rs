98927e921f4997dc0ff98743b59613aedf42a355
Digant Kasundra <digant@dropbox.com>
2017-03-17 00:40:47 -0700

    Add support for username override via headers

---
+++ src/req_headers.rs
@@ -0,0 +1,37 @@
+use rocket::http::{Header, Status};
+use rocket::Outcome;
+use rocket::request::{self, Request, FromRequest};
+
+use std;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct ReqHeaders {
+    pub origin: String,
+    pub headers: HashMap<String, String>,
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for ReqHeaders {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<ReqHeaders, ()> {
+        let keys: Vec<_> = request.headers().get("Origin").collect();
+
+        let origin;
+        if keys.len() != 1 {
+            //return Outcome::Failure((Status::BadRequest, ()));
+            origin = "unknown".into();
+        } else {
+            origin = keys[0].to_string();
+        }
+
+//        let origin = keys[0].to_string();
+        let mut headers = HashMap::new();
+
+        for myhead in request.headers().iter() {
+            headers.insert(myhead.name().into(), myhead.value().into());
+        }
+
+        return Outcome::Success(ReqHeaders { origin: origin, headers: headers });
+    }
+}
